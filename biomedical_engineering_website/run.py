f9320c05f99c7fdadc1b48611c7b02957b0d62f9
neba06 <nebyubelay06@gmail.com>
2025-02-13 12:47:59 +0300

    Update run.py

--- biomedical_engineering_website/run.py
+++ biomedical_engineering_website/run.py
@@ -4,8 +4,8 @@ app = create_app()
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)# Run the app 
+#if __name__ == "__main__":
+   # app.run(debug=True)# Run the app 
 from flask import Flask
 
 app = Flask(__name__)
